f25c49085c4107b1556811ba7b3ea966d0ce7b60
Jonathan Knapp <thann@purse.io>
2019-07-02 16:02:11 -0700

    Improve error handling

--- health.js
+++ health.js
@@ -5,6 +5,16 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
+
+class MaxTimeExceeded extends Error {
+  constructor(got, max) {
+    super();
+    this.got = got;
+    this.max = max;
+    this.message = `HealthError: (${got}) > (${max})`;
+  }
+};
+
 module.exports = {
   proveHealth(serviceName) {
     fs.writeFileSync(path.join(os.tmpdir(), `node-health-${serviceName}`), '');
@@ -16,18 +26,18 @@ module.exports = {
         - fs.statSync(path.join(os.tmpdir(),
                                `node-health-${serviceName}`)).mtimeMs
       ) / 1000 );
-    console.log(secondsSinceLastProof);
     if (maxSeconds && parseInt(maxSeconds) < secondsSinceLastProof) {
-      console.error('Max time exceded!');
-      process.exit(1);
+      throw new MaxTimeExceeded(secondsSinceLastProof, maxSeconds);
     }
+    return secondsSinceLastProof;
   },
   usage() {
     return (
 `Usage:
   ${path.basename(process.argv[1])} -p <service_name>               # Prove
   ${path.basename(process.argv[1])} -c <service_name> [max_seconds] # Check`);
-  }
+  },
+  MaxTimeExceeded: MaxTimeExceeded,
 };
 
 // Command-line interface
@@ -36,7 +46,18 @@ if (require.main === module) {
     console.error(module.exports.usage());
     process.exit(2);
   } else if (process.argv[2] === '-c') { // Check
-    module.exports.checkHealth(process.argv[3], process.argv[4]);
+    try {
+      const time = module.exports.checkHealth(process.argv[3], process.argv[4]);
+      console.log(time);
+    } catch(e) {
+      if (e instanceof MaxTimeExceeded) {
+        console.log(e.got);
+        console.error('Max time exceeded!');
+        process.exit(1);
+      } else {
+        throw e;
+      }
+    }
   } else if (process.argv[2] === '-p') { // Prove
     module.exports.proveHealth(process.argv[3]);
   } else {
